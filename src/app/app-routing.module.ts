2e918a3c23056fb098b622213d53e4585e363c9c
truonghan0812 <truonghan0812@gmail.com>
2019-12-14 12:38:21 +0700

    re-import dropdownlist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { CustomPipeComponent } from './custom-pipe/custom-pipe.component';
 
 const routes: Routes = [
   {path:'custom-pipe', component: CustomPipeComponent},
-  {path:'form-steps', loadChildren: () => import('./form-steps/form-steps.module').then(m => m.FormStepsModule) }
+  {path:'form-steps', loadChildren: () => import('./form-steps/form-steps.module').then(m => m.FormStepsModule) },
+  {path:'app-state-manage', loadChildren: () => import('./manage-state/manage-state.module').then(m => m.ManageStateModule) }
 ];
 
 @NgModule({
