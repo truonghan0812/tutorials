a0e924f0fefd6442fa28979838f47bd35c892a58
truonghan0812 <truonghan0812@gmail.com>
2019-12-21 22:30:16 +0700

    add actions handler

--- src/app/manage-state/manage-state/manage-state.component.ts
+++ src/app/manage-state/manage-state/manage-state.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-manage-state',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageStateComponent implements OnInit {
 
-  constructor() { }
+  public initItems:Array<String> = ['🍎','🍎','🍎','🍎','🍎'];
+  public items$:Observable<String[]>;
+  constructor() {}
 
   ngOnInit() {
+    this.items$ = of((this.initItems));
   }
 
+  addItem($event){
+    this.initItems.push('🍎');
+  }
+  removeItem($event){
+    this.initItems.pop();
+  }
+  refresh($event){
+    this.initItems = ['🍎','🍎','🍎','🍎','🍎'];
+    this.items$ = of((this.initItems));
+  }
 }
